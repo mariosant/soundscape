da4672beca6d91f05dd3a6c1e7e67cbaa8748dea
Marios Antonoudiou <mariosant@sent.com>
2020-05-11 17:39:43 +0000

    Add animation

--- src/components/Audiobutton/index.jsx
+++ src/components/Audiobutton/index.jsx
@@ -1,138 +1,126 @@
 import React from "react";
 import buzz from "musquito";
 import { cond, T } from "ramda";
-
+import { motion } from "framer-motion"
 import { styled } from "@material-ui/core/styles";
-import { withStyles, Slider, Box, IconButton } from "@material-ui/core";
+import { withStyles, Slider, Box, IconButton, LinearProgress } from "@material-ui/core";
 
 buzz.setup({
-	autoEnable: true,
+    autoEnable: true,
 });
 
 const StyledSlider = withStyles({
-	rail: {
-		color: "white",
-	},
-	track: {
-		color: "white",
-	},
-	thumb: {
-		color: "white",
-	},
-	disabled: {
-		opacity: 0.15,
-	},
+    rail: {
+        color: "white",
+    },
+    track: {
+        color: "white",
+    },
+    thumb: {
+        color: "white",
+    },
+    disabled: {
+        opacity: 0.15,
+    },
 })(Slider);
 
 const actionIs = (actionType) => (_, action) => actionType === action.type;
 
 const reducer = cond([
-	[actionIs("playstart"), (state) => ({ ...state, playing: true })],
-	[actionIs("stop"), (state) => ({ ...state, playing: false })],
-	[actionIs("loadprogress"), (state) => ({ ...state, loading: true })],
-	[actionIs("load"), (state) => ({ ...state, loading: false })],
-	[actionIs("volume"), (state, { payload }) => ({ ...state, volume: payload })],
-	[T, (state) => state],
+    [actionIs("playstart"), (state) => ({ ...state, playing: true })],
+    [actionIs("stop"), (state) => ({ ...state, playing: false })],
+    [actionIs("loadprogress"), (state) => ({ ...state, loading: true })],
+    [actionIs("load"), (state) => ({ ...state, loading: false })],
+    [actionIs("volume"), (state, { payload }) => ({ ...state, volume: payload })],
+    [T, (state) => state],
 ]);
 
 const StyledBox = styled(Box)(({ backgroundColor = "white", theme }) => {
-	return {
-		padding: "1rem",
-		color: "white",
-	};
+    return {
+        padding: "1rem",
+        color: "white",
+    };
 });
 
 const Audiobutton = (props) => {
-	const Icon = props.icon;
-	const [{ playing, loading, volume }, dispatch] = React.useReducer(reducer, {
-		playing: false,
-		loading: false,
-		volume: 8,
-	});
+    const Icon = props.icon;
+    const [{ playing, loading, volume }, dispatch] = React.useReducer(reducer, {
+        playing: false,
+        loading: false,
+        volume: 8,
+    });
 
-	const { current: buzzInstance } = React.useRef(
-		buzz({
-			src: props.src,
-			loop: true,
-			onplaystart: () => dispatch({ type: "playstart" }),
-			onstop: () => dispatch({ type: "stop" }),
-			onloadprogress: () => dispatch({ type: "loadprogress" }),
-			onload: () => dispatch({ type: "load" }),
-			onerror: console.error,
-			volume: 1,
-		})
-	);
+    const { current: buzzInstance } = React.useRef(
+        buzz({
+            src: props.src,
+            loop: true,
+            onplaystart: () => dispatch({ type: "playstart" }),
+            onstop: () => dispatch({ type: "stop" }),
+            onloadprogress: () => dispatch({ type: "loadprogress" }),
+            onload: () => dispatch({ type: "load" }),
+            onerror: console.error,
+            volume: 1,
+        })
+    );
 
-	const toggle = () =>
-		playing || loading ? buzzInstance.stop() : buzzInstance.play();
+    const toggle = () =>
+        playing || loading ? buzzInstance.stop() : buzzInstance.play();
 
-	const handleVolumeChange = (e, v) => {
-		e.preventDefault();
-		e.stopPropagation();
+    const handleVolumeChange = (e, v) => {
+        e.preventDefault();
+        e.stopPropagation();
 
-		dispatch({ type: "volume", payload: v });
-	};
+        dispatch({ type: "volume", payload: v });
+    };
 
-	React.useEffect(() => {
-		const gainNode = buzzInstance.gain();
-		gainNode.gain.value = volume;
-	}, [buzzInstance, volume]);
+    React.useEffect(() => {
+        const gainNode = buzzInstance.gain();
+        gainNode.gain.value = volume;
+    }, [buzzInstance, volume]);
 
-	return (
-		<StyledBox {...props}>
-			{!playing && !loading && (
-				<>
-					<IconButton onClick={toggle}>
-						<Icon
-							style={{
-								color: "white",
-								fontSize: "4rem",
-								opacity: 0.2,
-								filter: "blur",
-							}}
-						/>
-					</IconButton>
-					<Box display="flex">
-						<StyledSlider disabled value={volume} min={0} max={10} />
-					</Box>
-				</>
-			)}
+    const animationVariants = {
+        disabled: {
+            opacity: 0.2,
+            filter: 'blur(0)'
+        },
+        loading: {
+            opacity: 0.05
+            // filter: 'blur(7px)'
+        },
+        playing: {
+            opacity: 1,
+            scale: 1,
+            filter: 'blur(0px)'
 
-			{loading && (
-				<>
-					<IconButton onClick={toggle}>
-						<Icon
-							style={{
-								color: "white",
-								fontSize: "4rem",
-								opacity: 0.5,
-								filter: "blur",
-							}}
-						/>
-					</IconButton>
-					<Box display="flex">
-						<StyledSlider disabled value={volume} min={0} max={10} />
-					</Box>
-				</>
-			)}
-			{playing && (
-				<>
-					<IconButton onClick={toggle}>
-						<Icon style={{ color: "white", fontSize: "4rem" }} />
-					</IconButton>
-					<Box display="flex">
-						<StyledSlider
-							value={volume}
-							onChange={handleVolumeChange}
-							min={0}
-							max={10}
-							step={1}
-						/>
-					</Box>
-				</>
-			)}
-		</StyledBox>
-	);
+        }
+    }
+
+    const animationState = playing ? 'playing' : loading ? 'loading' : 'disabled'
+
+    return (
+        <StyledBox {...props}>
+            <motion.div animate={animationState} initial="disabled" variants={animationVariants}>
+                <IconButton onClick={toggle}>
+                    <Icon
+                        style={{
+                            color: "white",
+                            fontSize: "4rem",
+                        }}
+                    />
+                </IconButton>
+            </motion.div>
+            <Box display="flex">
+                <StyledSlider
+                    disabled={!playing}
+                    value={volume}
+                    onChange={handleVolumeChange}
+                    min={0}
+                    max={10}
+                    step={1}
+                />
+            </Box>
+        </StyledBox>
+    );
 };
 
 export default Audiobutton;
