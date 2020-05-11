3b68a87abccb8862880b80b4187ed38362f411c2
Marios Antonoudiou <mariosant@sent.com>
2020-05-11 07:59:20 +0000

    Add sticky header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import { Box, Typography, Container } from "@material-ui/core";
+import { Box, Typography, Container, styled } from "@material-ui/core";
+
+const StyledContainer = styled(Container)({
+	paddingBottom: "2rem",
+	paddingTop: "2rem",
+	position: "sticky",
+	top: 0,
+	zIndex: 1000,
+	backdropFilter: "blur(10px)",
+});
 
 export default function App() {
 	return (
-		<Container maxWidth="sm">
-			<Box textAlign="center" color="#888" marginY="2rem">
+		<StyledContainer maxWidth="sm">
+			<Box textAlign="center" color="#888">
 				<Typography variant="h4" component="h1">
 					soundscape
 				</Typography>
 			</Box>
-		</Container>
+		</StyledContainer>
 	);
 }
